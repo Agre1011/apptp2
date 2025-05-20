2c4dab529d1bffc6feef031ab9af4c502bd37546
Agre1011 <137312384+Agre1011@users.noreply.github.com>
2025-05-19 23:44:49 -0300

    Add files via upload

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TPMauiLoginApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
